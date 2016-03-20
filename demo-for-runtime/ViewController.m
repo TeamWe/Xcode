955ef1bae0ee79374397ef795f2c6116c73ff941
徐纪光 <xujiguang@xiyou-mobile-club.local>
2016-03-20 11:58:01 +0800

    third

--- demo-for-runtime/ViewController.m
+++ demo-for-runtime/ViewController.m
@@ -32,6 +32,7 @@
     ((void (*)(id, SEL , NSString*))objc_msgSend)((id)p, @selector(setName:),@"33");
     
     NSLog(@"fgewqr");
+//    afjlasdjgl;kjagl;kagla
 //    objc_msgSend(p,@selector(setAge:),20);
 }
 
